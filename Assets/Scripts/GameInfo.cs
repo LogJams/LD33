8390cf0e9bc40c9520ecbf0c6e546a3b0cfcc17b
Ian Mahuta <mahutai@msoe.edu>
2015-08-22 18:23:14 -0500

    Added initial GameInfo class.

---
+++ Assets/Scripts/GameInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+public static class GameInfo {
+
+	public static int bodyCount = 0;
+	public static int voicesLevel = 0;
+	public static int previousVoicesIncrement = 0;
+	public static int nightNumber = 0;
+	public enum Music{creepy};
+	public static Music music = Music.creepy;
+	public static int spawnInterval = 0;
+	
+
+}
