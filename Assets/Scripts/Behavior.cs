3746198a287baba4142844b529dafc71f9f89b08
Ian Mahuta <mahutai@msoe.edu>
2015-08-22 01:27:06 -0500

    Finished prototype
    Finished vision detection prototype.

--- Assets/Scripts/Behavior.cs
+++ Assets/Scripts/Behavior.cs
@@ -6,7 +6,7 @@ public class Behavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		vision = this.gameObject.GetComponent<Vision> ();
+		vision = GetComponent<Vision> ();
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Vision.cs
+++ Assets/Scripts/Vision.cs
@@ -5,10 +5,10 @@ using System.Collections.Generic;
 public class Vision : MonoBehaviour {
 	public bool inPeripheral;
 	public bool inMain;
-	int peripheralRange = 2;
-	int peripheralAngle = 25;
-	int mainRange = 4;
-	int mainAngle = 110;
+	float peripheralRange = 1.5f;
+	float peripheralAngle = 110;
+	float mainRange = 3;
+	float mainAngle = 25;
 
 	List<GameObject> inVision;
 
@@ -29,6 +29,7 @@ public class Vision : MonoBehaviour {
 		foreach (GameObject target in inVision){
 			vector = target.transform.position - this.gameObject.transform.position;
 			angle = Mathf.Rad2Deg * Mathf.Atan2(vector.y, vector.x);
+			angle = (angle + 360) % 360;
 			if (angle <= mainAngle) {
 				inMain = FireRayCast(vector, true);
 			} else if (angle <= peripheralAngle){
@@ -39,34 +40,34 @@ public class Vision : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		if (inVision.Contains(collider.gameObject) == false){
+		if (inVision.Contains(collider.gameObject) == false && collider.isTrigger == false){
 			inVision.Add(collider.gameObject);
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D collider){
-		if (inVision.Contains(collider.gameObject) == true){
+		if (inVision.Contains(collider.gameObject) == true && collider.isTrigger == false){
 			inVision.Remove(collider.gameObject);
 		}
 	}
 
 	bool FireRayCast(Vector2 vector, bool mainVision){
 		bool isVisible = false;
-		int castRange = mainRange;
-		RaycastHit hit;
+		float castRange = mainRange;
 
 		if (mainVision == false) {
 			castRange = peripheralRange;
 		}
-		for (int i = -2; i <= 2; i++) {
+		//for (int i = -2; i <= 2; i++) {
 		//	if (Physics.Raycast (new Ray(this.gameObject.transform.position, vector), castRange, hit) = true ){
 		//		if (hit.collider.CompareTag("player") = true){
 		//			isVisible = true;
 		//		}
 		//	}
-		}
-		if (Physics.Raycast (new Ray(this.gameObject.transform.position, vector), out hit, castRange) == true ){
-			if (hit.collider.CompareTag("player") == true){
+		//}
+		RaycastHit2D hit;
+		if ((hit = Physics2D.Raycast (transform.position, vector, castRange)).collider != null){
+			if (hit.collider.CompareTag("Player") == true){
 				isVisible = true;
 			}
 		}		
