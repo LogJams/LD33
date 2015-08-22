a094b170ca6c0fc551a7467c4254043e7687c17c
Logan <giantpandagod@gmail.com>
2015-08-22 00:10:43 -0500

    Added initial kidnapping
    Updated tags on humans and player, added initial kidnapping into the
    creeper van.

---
+++ Assets/Scripts/DumpBody.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DumpBody : MonoBehaviour {
+
+	int bodyCount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!other.CompareTag ("Player")) {
+			Destroy(other.gameObject);
+			bodyCount ++;
+		}
+	}
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,32 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerInput : MonoBehaviour {
-
+	//is the player currently dragging a body?
+	public bool dragging;
+	public GameObject draggedBody;
+	float dragDist = 1.5f;
+	//player movement speed
 	float speed = 3; //m/s
 
+	//list of targets in range to knock out
+	List<GameObject> targets;
+
+	//current rotation and velocity of the player
 	float rotation;
 	Vector3 velocity;
+
+	//players rigidbody - used for movement and collision
 	Rigidbody2D body;
 
-	// Use this for initialization
+	//Initialize variables
 	void Start () {
 		velocity = new Vector3 ();
 		body = GetComponent<Rigidbody2D> ();
+		targets = new List<GameObject> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		velocity.Set (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-		velocity.Normalize ();
+		//check if the player tries to grab a body
+		if (Input.GetButtonUp ("Jump")) {
+			if (!dragging) { //if we aren't currently dragging a body
+				//grab the nearest body in the list of targets
+				float minDist = Mathf.Infinity;
+				GameObject nearest = null;
+				//loop through all grabbable targets and select the closest
+				foreach (GameObject obj in targets) {
+					float dist = (transform.position - obj.transform.position).sqrMagnitude;
+					if (dist < minDist) {
+						minDist = dist;
+						nearest = obj;
+					}
+				}
+				draggedBody = nearest;
+				if (draggedBody != null) { //if we grabbed someone
+					draggedBody.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.None;
+				}
+			} else if (draggedBody != null) { //drop the body
+				draggedBody.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+				draggedBody = null;
+			}
+		}
 
+		dragging = draggedBody != null; //check if we're carrying anything
+
+		//set velocity direction from input
+		velocity.Set (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+		velocity.Normalize (); //normalize speed to a unit vector
+		if (dragging) {
+			velocity = velocity * 0.33f; //slow down the player if they're carrying a body
+		}
+
+		//calculate the vector from the player's position to the mouse
 		Vector3 direction = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
-		rotation = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		rotation = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg; //find the angle of the direction vector
 	}
 
 	void FixedUpdate() {
+		//rotate the player and move along the x/y axis
+		//rotation is done in transform because Z rotation is fixed in the rigidbody
 		transform.rotation = Quaternion.AngleAxis (rotation, Vector3.forward);
 		body.MovePosition(transform.position + velocity * Time.fixedDeltaTime * speed);
+		//move whatever we're dragging
+		if (draggedBody != null) {
+			draggedBody.transform.position = transform.position + transform.right * dragDist;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Human")) {
+			targets.Add (other.gameObject);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (targets.Contains(other.gameObject)) {
+			targets.Remove (other.gameObject);
+		}
 	}
 }
