adacce90108e7d7b74d30a80f597bccd5e469616
Ian Mahuta <mahutai@msoe.edu>
2015-08-22 00:19:46 -0500

    Added Behavior and Vision script
    Created Ian's Scene to add NPC's behavior and vision scripts with some
    basic implementation.

---
+++ Assets/Scripts/Behavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behavior : MonoBehaviour {
+	public Vision vision;
+
+	// Use this for initialization
+	void Start () {
+		vision = this.gameObject.GetComponent<Vision> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (vision.inMain == true){
+			Debug.Log("DETECTED");
+		} else if (vision.inPeripheral == true) {
+			Debug.Log("IN PERIPHERAL");
+		}
+	}
+}
---
+++ Assets/Scripts/Vision.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Vision : MonoBehaviour {
+	public bool inPeripheral;
+	public bool inMain;
+	int peripheralRange = 2;
+	int peripheralAngle = 25;
+	int mainRange = 4;
+	int mainAngle = 110;
+
+	List<GameObject> inVision;
+
+	// Use this for initialization
+	void Start () {
+		inPeripheral = false;
+		inMain = false;
+		inVision = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		inPeripheral = false;
+		inMain = false;
+		Vector2 vector;
+		float angle;
+
+		foreach (GameObject target in inVision){
+			vector = target.transform.position - this.gameObject.transform.position;
+			angle = Mathf.Rad2Deg * Mathf.Atan2(vector.y, vector.x);
+			if (angle <= mainAngle) {
+				inMain = FireRayCast(vector, true);
+			} else if (angle <= peripheralAngle){
+				inPeripheral = FireRayCast(vector, false);
+			}
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (inVision.Contains(collider.gameObject) == false){
+			inVision.Add(collider.gameObject);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider){
+		if (inVision.Contains(collider.gameObject) == true){
+			inVision.Remove(collider.gameObject);
+		}
+	}
+
+	bool FireRayCast(Vector2 vector, bool mainVision){
+		bool isVisible = false;
+		int castRange = mainRange;
+		RaycastHit hit;
+
+		if (mainVision == false) {
+			castRange = peripheralRange;
+		}
+		for (int i = -2; i <= 2; i++) {
+		//	if (Physics.Raycast (new Ray(this.gameObject.transform.position, vector), castRange, hit) = true ){
+		//		if (hit.collider.CompareTag("player") = true){
+		//			isVisible = true;
+		//		}
+		//	}
+		}
+		if (Physics.Raycast (new Ray(this.gameObject.transform.position, vector), out hit, castRange) == true ){
+			if (hit.collider.CompareTag("player") == true){
+				isVisible = true;
+			}
+		}		
+		return isVisible;
+		//return true;
+	}
+}
