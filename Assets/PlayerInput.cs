68a94cfefeaca446afb78c935e617c04f238bba1
Logan <giantpandagod@gmail.com>
2015-08-21 22:41:54 -0500

    Created initial scene with player movement
    Player can move and collide with other actors in the scene

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerInput : MonoBehaviour {
+
+	float speed = 5; //m/s
+
+	Vector3 velocity;
+	Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector3 ();
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity.Set (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+		velocity.Normalize ();
+	}
+
+	void FixedUpdate() {
+		body.MovePosition(transform.position + velocity * Time.fixedDeltaTime * speed);
+	}
+}
