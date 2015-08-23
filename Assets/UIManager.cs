5e10c2feb102d5719e957a99f6768f1cbe9ffcf1
Logan <giantpandagod@gmail.com>
2015-08-22 22:19:12 -0500

    Initial main menu
    Main menu with Start, Instructions, and Exit buttons.

---
+++ Assets/UIManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public void StartGame() {
+		Application.LoadLevel ("Master Scene");
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+}
