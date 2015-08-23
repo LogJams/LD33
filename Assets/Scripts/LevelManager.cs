628192a531ee56780ce5ae445087c0a04eb355b3
Ian Mahuta <mahutai@msoe.edu>
2015-08-22 22:17:44 -0500

    Added NPC spawning and movement.
    NPC spawning and movement works, but 3 NPCs are being spawned at the
    start for unknown reasons.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public float timeBetweenSpawns = 10f;
+	public float timeSinceSpawns;
+	public GameObject person;
+	// Use this for initialization
+	void Start () {
+		timeSinceSpawns = timeBetweenSpawns;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceSpawns += Time.deltaTime;
+		if (timeSinceSpawns > timeBetweenSpawns) {
+			SpawnNPC ();
+			timeSinceSpawns = 0f;
+		}
+	}
+
+	void SpawnNPC(){
+		GameObject npc = Instantiate (person);
+	}
+}
--- Assets/Scripts/NPC AI/MoveBetweenPoints.cs
+++ Assets/Scripts/NPC AI/MoveBetweenPoints.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MoveBetweenPoints : MonoBehaviour {
 
 	public Transform[] waypoints;
+	public enum MoveType{patrol, pass, wait};
+	public MoveType moveType;
 	public bool patrol;
 
 	int currentWaypoint;
@@ -19,32 +21,46 @@ public class MoveBetweenPoints : MonoBehaviour {
 
 
 	Rigidbody2D body;
+	GameObject[] gateways;
 
 
 	// Use this for initialization
 	void Start () {
 		velocity = new Vector3 ();
 		body = GetComponent<Rigidbody2D> ();
+		int rand = Random.Range (0, 100);
+
+		if (rand < 50) {
+			moveType = MoveType.pass;
+			Debug.Log ("Pass");
+		} else if (rand < 80) {
+			moveType = MoveType.patrol;
+			Debug.Log ("Patrol");
+		} else {
+			moveType = MoveType.wait;
+			Debug.Log ("wait");
+		}
+		GetPath ();
+		currentWaypoint = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (waypoints.Length > 0 & currentWaypoint < waypoints.Length) { //if we're moving toward a waypoint
-		//	Debug.Log (currentWaypoint + " waypoint");
-
+		if (waypoints.Length > 0 && currentWaypoint < waypoints.Length) { //if we're moving toward a waypoint
 			//if we're at the target, increment the current waypoint
 			if ((transform.position - waypoints[currentWaypoint].position).sqrMagnitude < 0.1f) {
 				currentWaypoint ++;
 			}
 			//if we're patrolling modulate the waypoint by the length
-			if (patrol) {
+			if (moveType == MoveType.patrol) {
 				currentWaypoint = currentWaypoint % waypoints.Length;
 			}
 			//move toward current waypoint
+			if (currentWaypoint < waypoints.Length) {
 			Vector3 targetPos = waypoints[currentWaypoint].position;
 			velocity = targetPos - transform.position;
 			velocity = velocity.normalized * speed;
-
+			}
 			//set desired rotation
 			desiredRot = Mathf.Atan2 (velocity.y, velocity.x) * Mathf.Rad2Deg;
 		}
@@ -60,6 +76,25 @@ public class MoveBetweenPoints : MonoBehaviour {
 			}
 			//rotate toward the desired value
 			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (0, 0, desiredRot), Time.fixedDeltaTime * rotSpeed);
+		}
+	}
+
+	void GetPath(){
+		GameObject pathfinders = GameObject.FindGameObjectWithTag ("Pathfinder");
+		Pathfinder[] paths = pathfinders.GetComponents<Pathfinder>();
+		GameObject[] chosenPath = paths[Random.Range(0, paths.Length)].path;
+
+		gateways = GameObject.FindGameObjectsWithTag ("Gateway");
+		int start = Random.Range (0, gateways.Length);
+		this.gameObject.transform.position = gateways [start].transform.position;
+
+		int pathLength = chosenPath.Length;
+		if (moveType != MoveType.pass) {
+			pathLength = Random.Range (0, chosenPath.Length);
+		}
+		waypoints = new Transform[pathLength];
+		for (int i = 0; i < pathLength; i++){
+			waypoints[i] = chosenPath[i].transform;
 		}
 	}
 }
